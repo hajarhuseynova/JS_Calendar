d35213d7b538e28009f58fab7bd7fde96e7437f8
hajar298 <hajarih@code.edu.az>
2023-12-09 16:17:23 +0400

    updatee

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const left = document.getElementById("left");
 const right = document.getElementById("right");
 const monthElement = document.querySelector(".month");
 const special = document.querySelector(".special");
+const weekday = document.querySelector(".weekdayP");
+const monthDay = document.querySelector(".monthDay");
+const eventDetails = document.getElementById("eventDetails");
+const cardBlack = document.querySelector(".card-black");
 
 const months = [
   "January",
@@ -17,6 +21,41 @@ const months = [
   "October",
   "November",
   "December",
+];
+
+const events = [
+  {
+    day: 5,
+    weekday: "Thursday",
+    month: "January",
+    type: "Republic",
+    name: "Republic Day",
+    color: "blue",
+  },
+  {
+    day: 15,
+    weekday: "Sunday",
+    month: "January",
+    type: "Public",
+    name: "Qurban bayrami",
+    color: "orange",
+  },
+  {
+    day: 7,
+    weekday: "Saturday",
+    month: "February",
+    type: "Public",
+    name: "Qurban bayrami",
+    color: "orange",
+  },
+  {
+    day: 15,
+    weekday: "Sunday",
+    month: "February",
+    type: "Republic",
+    name: "Bayram",
+    color: "blue",
+  },
 ];
 
 const daysInMonth = {
@@ -41,8 +80,25 @@ function updateMonthDays(month, days) {
     const dayElement = document.createElement("p");
     dayElement.classList.add("monthday");
     dayElement.textContent = i;
+    const event = events.find((e) => e.day === i && e.month === month);
+    if (event) {
+      const circle = document.createElement("div");
+      circle.classList.add("color", event.color);
+      dayElement.appendChild(circle);
+
+      dayElement.addEventListener("click", function () {
+        displayEventDetails(event);
+      });
+    }
+
     monthElement.appendChild(dayElement);
   }
+}
+
+function displayEventDetails(event) {
+  monthDay.textContent = event.month + ", " + event.day;
+  weekday.textContent = event.weekday;
+  cardBlack.style.display = "block";
 }
 
 let currentMonthIndex = months.indexOf("January");
